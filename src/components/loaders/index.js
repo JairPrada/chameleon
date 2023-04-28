292bcef74019b3f0b68f17f32bb825ec49317a5f
Jair Andres Prada Sierra <jpradas@fundaciongruposocial.co>
2023-04-28 12:31:30 -0500

    feat(components/loaders): adding splash screen anmation

--- src/components/loaders/index.js
+++ src/components/loaders/index.js
@@ -1,18 +1,29 @@
-import React from 'react'
-import Lottie from 'lottie-react-web';
-import animationData from './animations/chameleon.json';
-
-
+import React from "react";
+import Lottie from "react-lottie-player";
+import animationData from "../../animations/chameleon.json";
 
 export const Loader = () => {
     return (
-        <Lottie
-            options={{
-                animationData: animationData,
-                autoplay: true,
-                loop: true,
+        <div
+            style={{
+                width: "100%",
+                height: "100vh",
+                position: "fixed",
+                zIndex: "100",
+                top: "0",
+                left: "0",
+                background: "#d4d4d4",
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
             }}
-        />
-
-    )
-}
+        >
+            <Lottie
+                loop
+                animationData={animationData}
+                play
+                style={{ width: "82vw", height: "80vh" }}
+            />
+        </div>
+    );
+};
